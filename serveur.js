1f6c858595cde2c81b0bfda2b72a24aa9a48ca22
unknown <nouranmalidi@gmail.com>
2026-01-22 16:34:11 +0300

    J'ai relié les serveurs et l'application expressjs

--- serveur.js
+++ serveur.js
@@ -1,12 +1,32 @@
 // Ici je vais codder mon serveur
-
-const http = require('http');         // require ressemble a import pour importer un module. 
+// require ressemble a import pour importer un module. 
 // Le module HTTP est stocké dans la variable http.
 
-// Mintenant je créer mon serveir en utilisant le package HTTP
+const http = require('http');     
+
+// J'importe l'application express app.js
+const app = require('./app');
+
+// Je configure le numéro du port utilisé dans le serveur
+app.set('port', 3000);
+
+
+// Maintenant je créer mon serveir en utilisant le package HTTP
+
+const server = http.createServer(app)
+
+/*
 const server = http.createServer((req, res) => {
     res.end("Bonjour, je suis le serveur!");
-});
+}); */
+
+const date = new Date();
+
 
 // Je précise le port 
-server.listen(3000);   // Le serveur est disponible au port 3000
+server.listen(3000, () => {
+    console.log("Le serveur tourne sur le port 3000.");
+    console.log(date.toLocaleDateString());
+    console.log(date.getHours());
+});   // Le serveur est disponible au port 3000
+
