0ec124150149651da8b715419ede2133f8d25774
unknown <nouranmalidi@gmail.com>
2026-01-27 11:20:26 +0300

    modification de l'app avec l'ajout de tableau niveau client

--- app.js
+++ app.js
@@ -17,17 +17,39 @@ next(); // Pour indiquer de passer au middleware suivant
 
 
 // J'utilise un Middleware 2 pour retourner la réponse au client lorsqu'il fait le rêquete localhost:3000.
-app.use((req,res) => {
-    res.json({message: "Vous êtes bien sur le serveur MALIDI"})
-    // res.json envoie deja une réponse donc le 3 eme middleware ne s'afficherera pas
-    
+/*
+app.use((req,res,next) => {
+    res.json({message: "Vous êtes bien sur le serveur MALIDI"});
+       next();
+});
+*/
+
+app.use((req,res,next) => {
+    console.log("Bonjour, je suis le serveur!");
+    next();
 });
 
-app.use((req,res) => {
-    console.log("Bonjour, je suis le serveur!");
-    // Pas de next () car c'est le dernier middleware
-    // Le message ne s'affichera pas car y'a deja une reponse avec le middleware 2
+// Je dessine mes Routes avec Express 
+
+app.use('/api/fruit', (req, res) => {
+    console.log("Je passe dans la route /api/fruit")
+    
+    // Mon fruit
+    const fruit = [
+        {
+            id: 1,
+            nom: "pomme",
+            description: "fruit saisonier riche vitamin C",
+            prix: 3
+        }
+    ];
+
+    // En terme de réponse, je renvoie le tableau de fruits
+    res.json(fruit);
+
 });
+
+
 
 // J'exporte l'application express, ainsi l'app peut être importée dans d'autres fichiers JS.
 module.exports = app;
